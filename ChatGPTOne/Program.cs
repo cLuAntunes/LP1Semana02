1b309b7f055d452c0e7b49373630854207c109d6
Clara Antunes <claraantunes.work@gmail.com>
2024-02-28 20:59:42 +0000

    Add Loop

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -8,29 +8,33 @@ namespace ChatGPTOne
         {
             string ask, ans;
 
-            Console.Write("Ask me something!: ");
-            ask = Console.ReadLine();
+            while (true)
+            { 
 
-            switch (ask)
-            {
-                case "Is tomato a fruit?":
-                    ans = "Yes, it is.";
-                    break;
-                case "Why can't I sleep at night?":
-                    ans = "Because you're awake! I hope it helps!";
-                    break;
-                case "How are you so smart?":
-                    ans = "That's the trick! I'm not! >\u1D17\u2022!";
-                    break;                    
-                case "What is your biggest fear?":
-                    ans = "You leaving me...\u2565 \u1BC5 \u2565";
-                    break;
-                default:
-                    ans= "Sorry! I don't know about that!";
-                    break;
+                Console.Write("Ask me something!: ");
+                ask = Console.ReadLine();
+
+                switch (ask)
+                {
+                    case "Is tomato a fruit?":
+                        ans = "Yes, it is.";
+                        break;
+                    case "Why can't I sleep at night?":
+                        ans = "Because you're awake! I hope it helps!";
+                        break;
+                    case "How are you so smart?":
+                        ans = "That's the trick! I'm not! >\u1D17\u2022!";
+                        break;
+                    case "What is your biggest fear?":
+                        ans = "You leaving me...\u2565 \u1BC5 \u2565";
+                        break;
+                    default:
+                        ans = "Sorry! I don't know about that!";
+                        break;
+                }
+
+                Console.WriteLine(ans);
             }
-
-            Console.WriteLine(ans);
         }
     }
 }
