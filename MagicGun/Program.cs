55bb27336b0b82f9a6b5948a898c93f23ff03431
Clara Antunes <claraantunes.work@gmail.com>
2024-03-01 16:36:04 +0000

    Add Ask for Int and Conversion

---
+++ MagicGun/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro: ");
+            string numero = Console.ReadLine();
+
+            int n = int.Parse(numero);
+            
+        }
+    }
+}
